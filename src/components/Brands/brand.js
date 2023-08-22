d74e5d5231654a6f67c8503184e2e752a72ba3f9
harshmaan12 <138556258+harshmaan12@users.noreply.github.com>
2023-08-22 15:50:15 +0530

    optional Chaining fixed

--- src/components/Brands/brand.js
+++ src/components/Brands/brand.js
@@ -1,15 +1,13 @@
  import React from 'react';
 
 const Brand = ({imageUrls}) => {
- 
-
-  return (
+   return (
     <div className="text-left">
       <div className="relative lg:flex  ">
-       <h1 className="text-3xl font-bold mb-4 px-12 lg:absolute">Brands-style</h1>
+       <h1 className="text-3xl font-bold mb-4 px-12 lg:absolute">Brands</h1>
          <div className="flex overflow-x-auto p-7 m-2 gap-14 space-x-2 lg:overflow-x-hidden ">
         {
-        imageUrls.map((imageUrl, index) => (
+        imageUrls?.map((imageUrl, index) => (
           <img
             key={index}
             src={imageUrl}
