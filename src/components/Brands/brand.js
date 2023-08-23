1ed2f76f435aa98d5f3d73177a436a9d7c1b1601
harshmaan12 <138556258+harshmaan12@users.noreply.github.com>
2023-08-23 15:47:17 +0530

    Update changes

--- src/components/Brands/brand.js
+++ src/components/Brands/brand.js
@@ -4,8 +4,8 @@ const Brand = ({imageUrls}) => {
    return (
     <div className="text-left">
       <div className="relative lg:flex  ">
-       <h1 className="text-3xl font-bold mb-4 px-2 mt-2 lg:absolute">Brands</h1>
-         <div className="flex overflow-x-auto py-10   gap-12  space-x-2 lg:overflow-x-hidden ">
+       <h1 className="text-4xl font-extrabold mb-4 px-2 mt-2 lg:absolute">Brands</h1>
+         <div className="flex overflow-x-auto py-10 m-4  gap-12  space-x-2 lg:overflow-x-hidden ">
         {
         imageUrls?.map((imageUrl, index) => (
           <img
