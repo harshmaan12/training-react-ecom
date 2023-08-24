3479cd7ca6f70b239be154c4f56a3c125b32e05e
harshmaan12 <138556258+harshmaan12@users.noreply.github.com>
2023-08-24 16:05:56 +0530

    update done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className=" overflow-x-hidden lg:p-6">
+    <div className=" overflow-x-hidden  lg:p-6 ">
 
       <HomePageBanner></HomePageBanner>
      
